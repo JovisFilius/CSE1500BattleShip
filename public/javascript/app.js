1b5df7f38b823faea37b87b31f85d156921669a2
Milad Agha <m.agha@student.tudelft.nl>
2018-12-08 04:32:10 +0100

    Bug and indentation fixes

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,17 +1,22 @@
 var mouseOffset = null;
 var iMouseDown  = false;
 var lMouseState = false;
+
 var dragObject  = null;
 var curTarget   = null;
 var lastTarget  = null;
+var lastSelected = null;
 var dragHelper  = null;
+
 var cells = null;
 var activeCell = null;
 var lastCell = null;
+var base = null;
+var field = null;
 
 var fieldSize = 10;
-var shipOrientation = "horizontal";
-var shipSize = 0;
+var shipOrientation = null;
+var shipSize = null;
 var snappable = false;
 
 function mouseCoords(ev){
@@ -54,83 +59,79 @@ function calculatePositions(e){
 }
 
 var gridCellHoverIn = function(arg){
-  if(curTarget !== null){
-    var yCoord = arg.id.substring(0,1);
-    var xCoord = arg.id.substring(1, arg.id.length);
-    var newBackground = "lightgreen";
-    var largestCoord;
-    var startCoord = 1;
-    snappable = true;
-    if(shipOrientation === "horizontal"){
-      startCoord = parseInt(xCoord);
-    }else if (shipOrientation === "vertical"){
-      startCoord = yCoord.charCodeAt() - 64;
+  var yCoord = arg.id.substring(0,1);
+  var xCoord = arg.id.substring(1, arg.id.length);
+  var newBackground = "lightgreen";
+  var largestCoord;
+  var startCoord = 1;
+  snappable = true;
+  if(shipOrientation === "horizontal"){
+    startCoord = parseInt(xCoord);
+  }else if (shipOrientation === "vertical"){
+    startCoord = yCoord.charCodeAt() - 64;
+  }
+  largestCoord = startCoord + shipSize - 1;
+  if(largestCoord > fieldSize){
+    newBackground = "tomato";
+    snappable = false;
+  }
+  for(var i = 0; i<Math.min(curTarget.id.substring(5,6), fieldSize - startCoord + 1); i++){
+    var cellCoord;
+    if(shipOrientation === "horizontal" ){
+      cellCoord = parseInt(xCoord) + i;
+      var cell = document.getElementById(yCoord+cellCoord);
+    }else if(shipOrientation === "vertical"){
+      cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
+      var cell = document.getElementById(cellCoord+xCoord);
     }
-    largestCoord = startCoord + shipSize - 1;
-    if(largestCoord > fieldSize){
+    if(cell.getAttribute('occupied') === 'true'){
       newBackground = "tomato";
       snappable = false;
     }
-    for(var i = 0; i<Math.min(curTarget.id.substring(5,6), fieldSize - startCoord + 1); i++){
-      var cellCoord;
-      if(shipOrientation === "horizontal" ){
-        cellCoord = parseInt(xCoord) + i;
-        var cell = document.getElementById(yCoord+cellCoord);
-      }else if(shipOrientation === "vertical"){
-        cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
-        var cell = document.getElementById(cellCoord+xCoord);
-      }
-      if(cell.getAttribute('occupied') === 'true'){
-        newBackground = "tomato";
-        snappable = false;
-      }
-    }
+  }
 
-    for(var i = 0; i<Math.min(curTarget.id.substring(5,6), fieldSize - startCoord + 1); i++){
-      var cellCoord;
-      if(shipOrientation === "horizontal" ){
-        cellCoord = parseInt(xCoord) + i;
-        var cell = document.getElementById(yCoord+cellCoord);
-      }else if(shipOrientation === "vertical"){
-        cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
-        var cell = document.getElementById(cellCoord+xCoord);
-      }
-      if(cell.getAttribute('occupied') !== 'true'){
-          cell.style.backgroundColor = newBackground;
-      }
+  for(var i = 0; i<Math.min(curTarget.id.substring(5,6), fieldSize - startCoord + 1); i++){
+    var cellCoord;
+    if(shipOrientation === "horizontal" ){
+      cellCoord = parseInt(xCoord) + i;
+      var cell = document.getElementById(yCoord+cellCoord);
+    }else if(shipOrientation === "vertical"){
+      cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
+      var cell = document.getElementById(cellCoord+xCoord);
+    }
+    if(cell.getAttribute('occupied') !== 'true'){
+        cell.style.backgroundColor = newBackground;
     }
   }
 };
 
 var gridCellHoverOut = function(arg){
-  if(curTarget !== null){
-    var yCoord = arg.id.substring(0,1);
-    var xCoord = arg.id.substring(1, arg.id.length);
-    var newBackground = "white";
-    var largestCoord;
-    var startCoord = 1;
-    if(shipOrientation === "horizontal"){
-      startCoord = parseInt(xCoord);
-    }else if (shipOrientation === "vertical"){
-      startCoord = yCoord.charCodeAt() - 64;
-    }
-    largestCoord = startCoord + shipSize - 1;
-    for(var i = 0; i<Math.min(curTarget.id.substring(5,6), fieldSize - startCoord + 1); i++){
-      var cellCoord;
-      var cell;
-      if(shipOrientation === "horizontal" ){
-        cellCoord = parseInt(xCoord) + i;
-        cell = document.getElementById(yCoord+cellCoord);
-      }else if(shipOrientation === "vertical"){
-        cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
-        cell = document.getElementById(cellCoord+xCoord);
-      }
-      if(cell.getAttribute("occupied") !== "true"){
-        cell.style.backgroundColor = newBackground;
-      }
-    }
-    snappable = false;
+  var yCoord = arg.id.substring(0,1);
+  var xCoord = arg.id.substring(1, arg.id.length);
+  var newBackground = "white";
+  var largestCoord;
+  var startCoord = 1;
+  if(shipOrientation === "horizontal"){
+    startCoord = parseInt(xCoord);
+  }else if (shipOrientation === "vertical"){
+    startCoord = yCoord.charCodeAt() - 64;
   }
+  largestCoord = startCoord + shipSize - 1;
+  for(var i = 0; i<Math.min(curTarget.id.substring(5,6), fieldSize - startCoord + 1); i++){
+    var cellCoord;
+    var cell;
+    if(shipOrientation === "horizontal" ){
+      cellCoord = parseInt(xCoord) + i;
+      cell = document.getElementById(yCoord+cellCoord);
+    }else if(shipOrientation === "vertical"){
+      cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
+      cell = document.getElementById(cellCoord+xCoord);
+    }
+    if(cell.getAttribute("occupied") !== "true"){
+      cell.style.backgroundColor = newBackground;
+    }
+  }
+  snappable = false;
 };
 
 function mouseMove(ev){
@@ -138,21 +139,52 @@ function mouseMove(ev){
 	var target   = ev.target || ev.srcElement;
 	var mousePos = mouseCoords(ev);
   var dragObj = target.className;
+  calculatePositions(cells);
+  getPosition(base);
+  getPosition(field);
   
 	if(dragObj=='ship'){
+
 		if(iMouseDown && !lMouseState){
+
+      if(target.getAttribute('occupies') !== null){
+
+        var oldCell_id = target.getAttribute('occupies');
+        var oldCell = document.getElementById(oldCell_id);
+
+        var yCoord = oldCell.id.substring(0,1);
+        var xCoord = oldCell.id.substring(1, target.id.length);
+
+        for(var i = 0; i<target.id.substring(5,6); i++){
+          var cellCoord;
+          var cell;
+          
+          if(shipOrientation === "horizontal" ){
+            cellCoord = parseInt(xCoord) + i;
+            cell = document.getElementById(yCoord+cellCoord);
+          }
+          else if(shipOrientation === "vertical"){
+            cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
+            cell = document.getElementById(cellCoord+xCoord);
+          }
+
+          cell.setAttribute("occupied", "false");
+        }
+
+      }
 
       shipSize = parseInt(target.id.substring(5,6));
       if (target.getAttribute('shipOrientation') === null) target.setAttribute('shipOrientation', 'horizontal');
       shipOrientation = target.getAttribute('shipOrientation');
 
-      if(lastTarget !== null){
-        lastTarget.style.border = '4px solid black';
+      if(lastSelected !== null){
+        lastSelected.style.border = '4px solid black';
       }
       target.style.border = '4px solid red';
 
       mouseOffset   = getMouseOffset(target, ev);
       
+      document.body.appendChild(dragHelper);
       for(var i=0; i<dragHelper.childNodes.length; i++) dragHelper.removeChild(dragHelper.childNodes[i]);
       var copy = target.cloneNode(true);
       copy.style.top = '0px';
@@ -163,74 +195,138 @@ function mouseMove(ev){
       dragHelper.style.display = '';
       target.style.display = 'none';
       
+      lastSelected = target;
       curTarget = target;
+
 		}
   }
   
 	if(curTarget){
+
     var cur_x = mousePos.x - mouseOffset.x;
     var cur_y = mousePos.y - mouseOffset.y;
+
     dragHelper.style.top  = cur_y + 'px';
     dragHelper.style.left = cur_x + 'px';
+
     for(let i = 0; i < cells.length; i++) {
+
       if((parseInt(cells[i].getAttribute('x_start')) < cur_x + 20) &&
-					(parseInt(cells[i].getAttribute('y_start')) < cur_y + 20) &&
-					(parseInt(cells[i].getAttribute('x_start')) + 40  > cur_x + 20) &&
-					(parseInt(cells[i].getAttribute('y_start'))  + 40 > cur_y + 20)){
-            activeCell = cells[i];
-            if(lastCell !== activeCell){
-              if(lastCell !== null){
-                gridCellHoverOut(lastCell);
-              }
-              gridCellHoverIn(activeCell);
-              lastCell = activeCell;
-            }
-						break;
-				}
+      (parseInt(cells[i].getAttribute('y_start')) < cur_y + 20) &&
+      (parseInt(cells[i].getAttribute('x_start')) + 40  > cur_x + 20) &&
+      (parseInt(cells[i].getAttribute('y_start'))  + 40 > cur_y + 20)){
+        activeCell = cells[i];
+        if(lastCell !== activeCell){
+          if(lastCell !== null){
+            gridCellHoverOut(lastCell);
+          }
+          gridCellHoverIn(activeCell);
+          lastCell = activeCell;
+        }
+        break;
+      }
     }
+
+    if((parseInt(base.getAttribute('x_start')) - 130 < cur_x + 20) &&
+      (parseInt(base.getAttribute('y_start')) < cur_y + 20) &&
+      (parseInt(base.getAttribute('x_start')) + 500  > cur_x + 20) &&
+      (parseInt(base.getAttribute('y_start'))  + 500 > cur_y + 20)){
+        if(lastCell !== null){
+          gridCellHoverOut(lastCell);
+        }
+        lastCell = null;
+    }
+
   }
   
 	lMouseState = iMouseDown;
 	lMouseState = iMouseDown;
 	return false;
 }
+
 function mouseUp(ev){
+
 	if(curTarget){
-		dragHelper.style.display = 'none';
+
+    document.body.removeChild(dragHelper);
     curTarget.style.display = '';
+
     if (lastCell !== null) {
+
       if(!snappable){
         gridCellHoverOut(lastCell);
       }
       else{
+
         var parent = curTarget.parentNode;
         parent.removeChild(curTarget);
-        document.body.appendChild(curTarget);
-        curTarget.style.left = lastCell.getAttribute('x_start') + 'px';
-        curTarget.style.top = lastCell.getAttribute('y_start') + 'px';
+        field.appendChild(curTarget);
+
+        curTarget.style.left = lastCell.getAttribute('x_start') - field.getAttribute('x_start') + 'px';
+        curTarget.style.top = lastCell.getAttribute('y_start') - field.getAttribute('y_start') + 'px';
+
+        curTarget.setAttribute('occupies', lastCell.id);
+
+        var yCoord = lastCell.id.substring(0,1);
+        var xCoord = lastCell.id.substring(1, curTarget.id.length);
+
+        for(var i = 0; i<curTarget.id.substring(5,6); i++){
+          var cellCoord;
+          var cell;
+
+          if(shipOrientation === "horizontal" ){
+            cellCoord = parseInt(xCoord) + i;
+            cell = document.getElementById(yCoord+cellCoord);
+          }
+          else if(shipOrientation === "vertical"){
+            cellCoord = String.fromCharCode(yCoord.charCodeAt() + i);
+            cell = document.getElementById(cellCoord+xCoord);
+          }
+
+          cell.setAttribute("occupied", "true");
+        }
       }
     }
+    else {
+
+      if(curTarget.getAttribute('occupies') !== null){
+
+        field.removeChild(curTarget);
+        curTarget.removeAttribute('occupies');
+        base.appendChild(curTarget);
+
+        curTarget.style.left = curTarget.getAttribute('x') + 'px'; 
+        curTarget.style.top = curTarget.getAttribute('y') + 'px';
+      }
+
+    }
   }
+  
 	lastTarget  = curTarget;
 	curTarget  = null;
   lastCell = null;
 	iMouseDown = false;
 }
+
 function mouseDown(){
 	iMouseDown = true;
 	if(lastTarget){
 		return false;
 	}
 }
+
 document.onmousemove = mouseMove;
 document.onmousedown = mouseDown;
 document.onmouseup   = mouseUp;
+
 window.onload = function(){
-    dragHelper = document.createElement('DIV');
-    dragHelper.style.cssText = 'position:absolute;display:none;';
-    document.body.appendChild(dragHelper);
-    cells = document.getElementsByClassName('gridCell');
-    calculatePositions(cells);
+
+  dragHelper = document.createElement('DIV');
+  dragHelper.style.cssText = 'position:absolute;display:none;';
+  
+  cells = document.getElementsByClassName('gridCell');
+  base = document.getElementsByClassName('other')[0];
+  field = document.getElementsByClassName('player')[0];
 }
 
 
