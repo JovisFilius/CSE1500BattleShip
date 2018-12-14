db2b5585fdd91fc080063cb47f2bf1f5206b6e2a
JovisFilius <JovisFilius@users.noreply.github.com>
2018-12-14 20:51:06 +0100

    progress

---
+++ public/javascript/socket.js
@@ -0,0 +1,12 @@
+var socket = new WebSocket("ws://localhost:4444");
+socket.onmessage = function(event){
+    console.log(event.data);
+}
+
+if(document.getElementById('play') !== null){
+    document.getElementById('play').onclick = function(event){
+        var playerData = "data containing board positions and player info";
+        socket.send(playerData);
+        console.log('clicked play button, sending data to server');
+    }
+}
