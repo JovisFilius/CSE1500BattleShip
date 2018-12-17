f2eb5bb8f02028c47c4f6dcdea92d97cac1ca46e
JovisFilius <42693100+JovisFilius@users.noreply.github.com>
2018-12-17 12:37:52 +0100

    Added player 2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var websocket = require("ws");
 
 var indexRouter = require('./routes/index');
 
+var gameStats = require('./stats');
+
 var port  = process.argv[2];
 var app = express();
 
@@ -14,7 +16,8 @@ const wss = new websocket.Server({ server });
 
 wss.on("connection", function connection(ws) {
     // console.log('Greeting the player');
-    ws.send("Hello player");
+    // ws.send("Hello player");
+    ws.send(JSON.stringify(gameStats));
     ws.on('message', function incoming(message) {
 
         console.log('received message from client: %s', message);
